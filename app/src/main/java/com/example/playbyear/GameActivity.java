1074db55e83fbd25b5389cc913b2a15b17ba07bd
mattiaspalmgren <mattiaspalmgren92@gmail.com>
2015-11-17 16:39:32 +0100

    baseline for the gameactivity

--- app/src/main/java/com/example/playbyear/GameActivity.java
+++ app/src/main/java/com/example/playbyear/GameActivity.java
@@ -1,29 +1,19 @@
 package com.example.playbyear;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        String[] items = new String[] {"Deportees","First Aid Kit","Robyn","De vet du"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.opt_row, R.id.text1, items);
+        setListAdapter(adapter);
     }
-
 }
