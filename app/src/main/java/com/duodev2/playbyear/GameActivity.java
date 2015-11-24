3789685c753b00532e0fe298f1fde0ef87a24dd3
petraohlin8 <petraohlin8@gmail.com>
2015-11-24 17:00:13 +0100

    added simple db

--- app/src/main/java/com/duodev2/playbyear/GameActivity.java
+++ app/src/main/java/com/duodev2/playbyear/GameActivity.java
@@ -7,16 +7,32 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.List;
+
 public class GameActivity extends ListActivity {
+
+    private MusicDbHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        String[] items = new String[] {"Deportees","First Aid Kit","Robyn","De vet du"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.opt_row, R.id.text1, items);
+        db = new MusicDbHelper(this);
+
+        // add default items
+        db.addMusicItem(new MusicItem("Hello Saferide", "My Best Friend", "Swedish Pop", "spotify:track:2WYUZcrkZuyXGgKE05UhEC"));
+        db.addMusicItem(new MusicItem("Miss Li", "Dancing the Whole Way Home", "Swedish Pop", "spotify:track:67tmclJQToXoCjiDCm43Jg"));
+        db.addMusicItem(new MusicItem("Marit Bergman", "This Is The Year", "Swedish Pop", "spotify:track:6rBzSjdrihboUkZ0YyhWLc"));
+        db.addMusicItem(new MusicItem("Maia Hirasawa", "I Found This Boy", "Swedish Pop", "spotify:track:2unK0jqfq1VvN1J4FxB3CL"));
+
+
+        // get all music
+        List<String> songs = db.getAllSongs();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.opt_row, R.id.text1, songs);
         setListAdapter(adapter);
+
     }
 
     @Override
